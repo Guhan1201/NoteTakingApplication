606c9670484ac86d0d62c59a429c5cd4bf23cc8c
ganesh-9276 <ganeshsuriya24@gmail.com>
2020-03-19 16:01:59 +0530

    Nav graph changes

--- app/src/main/java/com/example/roompracticeactivity/database/dao/NotesDao.kt
+++ app/src/main/java/com/example/roompracticeactivity/database/dao/NotesDao.kt
@@ -1,19 +1,20 @@
-package com.example.roompracticeactivity
+package com.example.roompracticeactivity.database.dao
 
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import com.example.roompracticeactivity.database.entities.Notes
 
 @Dao
 interface NotesDao {
 
-    @Query("SELECT * from notes_table ")
-    fun getAllNotes() : LiveData<List<Notes>>
+    @Query("SELECT * FROM notes_table ORDER BY last_modified_time DESC")
+    fun getAllNotes(): LiveData<List<Notes>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insert(notes:  Notes)
+    suspend fun insert(notes: Notes)
 
     @Query("DELETE FROM notes_table")
     suspend fun deleteAll()
--- app/src/main/java/com/example/roompracticeactivity/viewmodel/NotesListViewModel.kt
+++ app/src/main/java/com/example/roompracticeactivity/viewmodel/NotesListViewModel.kt
@@ -1,25 +1,30 @@
-package com.example.roompracticeactivity
+package com.example.roompracticeactivity.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.viewModelScope
+import com.example.roompracticeactivity.database.NotesRoomDatabase
+import com.example.roompracticeactivity.database.entities.Notes
+import com.example.roompracticeactivity.database.repository.NotesRepository
 import kotlinx.coroutines.launch
 
 class NotesListViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val repository: notesRepository
+    private val repository: NotesRepository
 
     val allNotes: LiveData<List<Notes>>
 
     init {
         val wordsDao = NotesRoomDatabase.getDatabase(application).notesDao()
-        repository = notesRepository(wordsDao)
+        repository = NotesRepository(wordsDao)
         allNotes = repository.allNotes
     }
 
     fun insert(notes: Notes) = viewModelScope.launch {
         repository.insert(notes)
     }
+
+
 
 }
