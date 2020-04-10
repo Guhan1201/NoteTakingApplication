309044414b9a02be19cad1a0c9bfd7cc6bb6b9f0
guhanmuthu-selvaraj <guhanmuthu.selvaraj@freshworks.com>
2020-04-10 22:23:51 +0530

    Missed line of code

--- app/src/main/java/com/example/roompracticeactivity/fragment/AddNoteFragment.kt
+++ app/src/main/java/com/example/roompracticeactivity/fragment/AddNoteFragment.kt
@@ -9,6 +9,7 @@ import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.LinearLayout
 import androidx.annotation.RequiresApi
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.fragment.app.Fragment
 import com.example.roompracticeactivity.R
 import com.example.roompracticeactivity.config
@@ -25,6 +26,9 @@ class AddNoteFragment : Fragment() {
     private lateinit var notesTitle: EditText
     private lateinit var notesDescription: EditText
     private lateinit var repository: NotesRepository
+    private lateinit var button: LinearLayout
+    private lateinit var back: LinearLayout
+    private lateinit var parent: CoordinatorLayout
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,35 +39,51 @@ class AddNoteFragment : Fragment() {
     }
 
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-
-
-        notesTitle = view.findViewById(R.id.edit_word)
-        notesDescription = view.findViewById(R.id.description)
-
-        val wordsDao = NotesRoomDatabase.getDatabase(requireContext()).notesDao()
-        repository = NotesRepository(wordsDao)
-
-
-        val button = view.findViewById<LinearLayout>(R.id.save)
+    override fun onStart() {
         button.setOnClickListener {
-
             if (TextUtils.isEmpty(notesTitle.text)) {
 
                 val snack = Snackbar.make(
-                    view.findViewById(R.id.parent),
-                    "Hey, this is a MD2 toast!",
+                    parent,
+                    getString(R.string.title_should_not_be_empty),
                     Snackbar.LENGTH_LONG
                 )
+
                 snack.config(requireContext())
                 snack.show()
-
             } else {
                 insertNotes()
                 activity?.onBackPressed()
 
             }
         }
+        back.setOnClickListener {
+            activity?.onBackPressed()
+        }
+        super.onStart()
+    }
+
+    override fun onStop() {
+        button.setOnClickListener(null)
+        back.setOnClickListener(null)
+        super.onStop()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+
+        notesTitle = view.findViewById(R.id.edit_word)
+        notesDescription = view.findViewById(R.id.description)
+        parent = view.findViewById(R.id.parent)
+
+        val wordsDao = NotesRoomDatabase.getDatabase(requireContext()).notesDao()
+        repository = NotesRepository(wordsDao)
+
+
+        button = view.findViewById(R.id.save)
+        back = view.findViewById(R.id.back)
+
     }
 
     private fun insertNotes() {
