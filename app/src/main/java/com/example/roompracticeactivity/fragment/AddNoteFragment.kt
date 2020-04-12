be89bc45d72889d71a04d9676361dff8543ccb49
guhanmuthu-selvaraj <guhanmuthu.selvaraj@freshworks.com>
2020-04-12 19:36:53 +0530

    initial commit for the color picker

--- app/src/main/java/com/example/roompracticeactivity/fragment/AddNoteFragment.kt
+++ app/src/main/java/com/example/roompracticeactivity/fragment/AddNoteFragment.kt
@@ -1,5 +1,6 @@
 package com.example.roompracticeactivity.fragment
 
+import android.annotation.SuppressLint
 import android.os.Build
 import android.os.Bundle
 import android.text.TextUtils
@@ -7,7 +8,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.LinearLayout
+import androidx.annotation.ColorRes
 import androidx.annotation.RequiresApi
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.fragment.app.Fragment
@@ -17,6 +20,7 @@ import com.example.roompracticeactivity.database.NotesRoomDatabase
 import com.example.roompracticeactivity.database.entities.Notes
 import com.example.roompracticeactivity.database.repository.NotesRepository
 import com.google.android.material.snackbar.Snackbar
+import dev.sasikanth.colorsheet.ColorSheet
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
@@ -29,6 +33,14 @@ class AddNoteFragment : Fragment() {
     private lateinit var button: LinearLayout
     private lateinit var back: LinearLayout
     private lateinit var parent: CoordinatorLayout
+    private lateinit var colorPallete: ImageView
+    private lateinit var colors: IntArray
+    private lateinit var parentLinearLayout : LinearLayout
+
+    private var selectedColor: Int = ColorSheet.NO_COLOR
+    private var noColorOption = false
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -42,13 +54,11 @@ class AddNoteFragment : Fragment() {
     override fun onStart() {
         button.setOnClickListener {
             if (TextUtils.isEmpty(notesTitle.text)) {
-
                 val snack = Snackbar.make(
                     parent,
                     getString(R.string.title_should_not_be_empty),
                     Snackbar.LENGTH_LONG
                 )
-
                 snack.config(requireContext())
                 snack.show()
             } else {
@@ -59,6 +69,23 @@ class AddNoteFragment : Fragment() {
         back.setOnClickListener {
             backPressed()
         }
+
+
+        colorPallete.setOnClickListener {
+            ColorSheet().cornerRadius(8)
+                .colorPicker(
+                    colors = colors,
+                    noColorOption = true,
+                    selectedColor = selectedColor,
+                    listener = { color ->
+                        selectedColor = color
+                        setColor(color)
+                        print("Guhan Test$color")
+                    })
+                .show(requireActivity().supportFragmentManager)
+        }
+
+
         super.onStart()
     }
 
@@ -75,13 +102,15 @@ class AddNoteFragment : Fragment() {
         notesTitle = view.findViewById(R.id.edit_word)
         notesDescription = view.findViewById(R.id.description)
         parent = view.findViewById(R.id.parent)
+        colors = resources.getIntArray(R.array.colors)
+        parentLinearLayout = view.findViewById(R.id.parentLinearLayout)
+
 
         val wordsDao = NotesRoomDatabase.getDatabase(requireContext()).notesDao()
         repository = NotesRepository(wordsDao)
-
-
         button = view.findViewById(R.id.save)
         back = view.findViewById(R.id.back)
+        colorPallete = view.findViewById(R.id.colorPallete)
 
     }
 
@@ -104,4 +133,10 @@ class AddNoteFragment : Fragment() {
     private fun backPressed() {
         activity?.onBackPressed()
     }
+
+    @SuppressLint("ResourceAsColor")
+    private fun setColor(@ColorRes color : Int) {
+       parentLinearLayout.setBackgroundColor(color)
+    }
+
 }
