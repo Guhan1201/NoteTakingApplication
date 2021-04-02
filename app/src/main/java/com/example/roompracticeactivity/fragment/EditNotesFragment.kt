c657bd5b969db8ade1981da19a09753261509329
guhanmuthu <guhanmuthu.selvaraj@freshworks.com>
2021-04-02 22:34:35 +0530

    created fragment and xml for edit notes

---
+++ app/src/main/java/com/example/roompracticeactivity/fragment/EditNotesFragment.kt
@@ -0,0 +1,8 @@
+package com.example.roompracticeactivity.fragment
+
+import androidx.fragment.app.Fragment
+import com.example.roompracticeactivity.R
+
+class EditNotesFragment : Fragment(R.layout.fragment_edit_notes) {
+
+}
