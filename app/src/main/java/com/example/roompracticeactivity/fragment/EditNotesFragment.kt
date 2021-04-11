748c3518e78c6ede36a33a2f247f92565bbf64e1
guhanmuthu <guhanmuthu.selvaraj@freshworks.com>
2021-04-11 13:48:14 +0530

    handled corner cases

--- app/src/main/java/com/example/roompracticeactivity/fragment/EditNotesFragment.kt
+++ app/src/main/java/com/example/roompracticeactivity/fragment/EditNotesFragment.kt
@@ -1,27 +1,42 @@
 package com.example.roompracticeactivity.fragment
 
 import android.annotation.SuppressLint
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.graphics.Paint
+import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.widget.DatePicker
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TimePicker
 import androidx.annotation.ColorRes
+import androidx.annotation.RequiresApi
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.work.Data
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
 import com.example.roompracticeactivity.R
 import com.example.roompracticeactivity.database.entities.Notes
 import com.example.roompracticeactivity.fragment.AddNoteFragment.Companion.DATE_TIME_COMPONENT_FORMAT
 import com.example.roompracticeactivity.viewmodel.NotesListViewModel
+import com.example.roompracticeactivity.workmanager.RemainderWorker
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import dev.sasikanth.colorsheet.ColorSheet
 import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.TimeUnit
 
-class EditNotesFragment : Fragment(R.layout.fragment_edit_notes) {
+class EditNotesFragment : Fragment(R.layout.fragment_edit_notes),
+    DatePickerDialog.OnDateSetListener,
+    TimePickerDialog.OnTimeSetListener {
 
     companion object {
         val NOTES = "notes"
@@ -37,12 +52,17 @@ class EditNotesFragment : Fragment(R.layout.fragment_edit_notes) {
     private lateinit var colors: IntArray
     private lateinit var notes: Notes
     private lateinit var setRemainder: ExtendedFloatingActionButton
+    private var calenderScheduled = false
+    private lateinit var dateTime: DateTime
+    private lateinit var calendar: Calendar
+
 
     private var selectedColor: Int = ColorSheet.NO_COLOR
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        dateTime = DateTime()
         notesViewModel = ViewModelProvider(this).get(NotesListViewModel::class.java)
         notesTitle = view.findViewById(R.id.edit_word)
         notesDescription = view.findViewById(R.id.description)
@@ -60,21 +80,35 @@ class EditNotesFragment : Fragment(R.layout.fragment_edit_notes) {
             val date = notes.alaram_time
             val format = SimpleDateFormat(DATE_TIME_COMPONENT_FORMAT)
             setRemainder.text = format.format(date)
-            setRemainder.paintFlags= setRemainder.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+            if (date != null) {
+                if (date < System.currentTimeMillis()) {
+                    setRemainder.paintFlags = setRemainder.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+                }
+            }
         }
 
         parent.setBackgroundColor(selectedColor)
         toolbar.setBackgroundColor(selectedColor)
         save.setOnClickListener {
-            val updatedNotes = notes.copy(
+            var updatedNotes = notes.copy(
                 notesTitle = notesTitle.text.toString(),
                 description = notesDescription.text.toString(),
                 backgroundColor = selectedColor,
                 last_modified_time = System.currentTimeMillis()
             )
+            if (calenderScheduled) {
+                updatedNotes = updatedNotes.copy(alaram_time = calendar.timeInMillis)
+            }
             notesViewModel.updateNotes(updatedNotes)
+            setRemainderNotification()
             backPressed()
         }
+        setRemainder.setOnClickListener {
+            val datePicker: DialogFragment =
+                com.example.roompracticeactivity.picker.DatePicker(this)
+            datePicker.show(requireActivity().supportFragmentManager, datePicker.tag)
+        }
+
         colorPallete.setOnClickListener {
             ColorSheet().cornerRadius(8)
                 .colorPicker(
@@ -106,6 +140,21 @@ class EditNotesFragment : Fragment(R.layout.fragment_edit_notes) {
         }
     }
 
+    private fun setRemainderNotification() {
+        val data = Data.Builder()
+            .putLong(AddNoteFragment.UNIQUE_ID, notes.createdTime)
+            .build()
+        if (this::calendar.isInitialized) {
+            val timeDiff = calendar.timeInMillis - System.currentTimeMillis()
+            val dailyWorkRequest = OneTimeWorkRequest.Builder(RemainderWorker::class.java)
+                .setInputData(data)
+                .setInitialDelay(timeDiff, TimeUnit.MILLISECONDS)
+                .build()
+            WorkManager.getInstance(requireContext()).enqueue(dailyWorkRequest)
+        }
+
+    }
+
 
     private fun backPressed() {
         findNavController().navigateUp()
@@ -118,4 +167,37 @@ class EditNotesFragment : Fragment(R.layout.fragment_edit_notes) {
     }
 
 
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        view?.isShown?.let {
+            dateTime = dateTime.copy(year = year, month = month + 1, dayOfMonth = dayOfMonth)
+        }
+        val timePicker: DialogFragment = com.example.roompracticeactivity.picker.TimePicker(this)
+        timePicker.show(requireActivity().supportFragmentManager, timePicker.tag)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    @SuppressLint("SimpleDateFormat")
+    override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
+        view?.isShown?.let {
+            dateTime = dateTime.copy(hourOfDay = hourOfDay, minute = minute)
+            calendar = Calendar.getInstance()
+            calendar.set(
+                dateTime.getYear(),
+                dateTime.getMonth() - 1,
+                dateTime.getDayOfMonth(),
+                dateTime.getHourOfDay(),
+                dateTime.getMinute()
+            )
+            val date = calendar.time
+            val format = SimpleDateFormat(DATE_TIME_COMPONENT_FORMAT)
+            setRemainder.text = format.format(date)
+            if (calendar.timeInMillis <= System.currentTimeMillis()) {
+                setRemainder.paintFlags = setRemainder.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+            } else {
+                setRemainder.paintFlags = 0
+            }
+            calenderScheduled = true
+        }
+
+    }
 }
